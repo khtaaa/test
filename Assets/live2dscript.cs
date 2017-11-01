1848bf56ee21a2127d68ea62c15eb43381d4875d
orika <adadad7297@yahoo.co.jp>
2017-11-01 16:11:00 +0900

    live2dのtestを開始

---
+++ Assets/live2dscript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using live2d;
+
+public class live2dscript : MonoBehaviour {
+	public TextAsset moc;
+	public Texture2D[] texture;
+	private Live2DModelUnity live2Dmodel;
+
+	// Use this for initialization
+	void Start () {
+		
+		//live2d初期化
+		Live2D.init ();
+
+		//モデルデータ読み込み？
+		live2Dmodel = Live2DModelUnity.loadModel (moc.bytes);
+
+		//テクスチャー読み込み
+		live2Dmodel.setTexture (0, texture);
+
+		//モデルの幅を獲得
+		float modelwidth = live2Dmodel.getCanvasWidth ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
